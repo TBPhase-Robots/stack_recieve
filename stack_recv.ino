3aa78a306f04a1bea2d3e045b082de391b068325
TBExtent <ef18558@bristol.ac.uk>
2022-07-12 09:40:34 +0100

    commented

--- stack_recv.ino
+++ stack_recv.ino
@@ -8,24 +8,24 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-#include <std_msgs/msg/float32.h>
-#include <std_msgs/msg/int64.h>
 #include <geometry_msgs/msg/vector3.h>
 
 #include <Wire.h>           // i2c to connect to IR communication board.
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+//  ROS error handlers. Calls error_loop if check fails.
+#define RCCHECK(fn) {rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) {rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
 
 #define ROBOT_I2C_ADDR  8
 
+//  Stack screen width + height
 #define WIDTH 320
 #define HEIGHT 240
 
 // Data to send(tx) and receive(rx)
 // on the i2c bus.
 // Needs to match the master device
-
 #pragma pack(1)
 typedef struct i2c_status {
   float x;                  // 4 bytes
@@ -35,18 +35,18 @@ typedef struct i2c_status {
 } i2c_status_t;
 #pragma pack()
 
+//  Data sent to and from the 3Pi robot
 i2c_status_t i2c_status_tx;
 i2c_status_t i2c_status_rx;
 
-rcl_subscription_t subscriber;
 rcl_subscription_t vector_subscriber;
-std_msgs__msg__Float32 msg;
 geometry_msgs__msg__Vector3 vector_msg;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
 rclc_executor_t executor;
 
+//  Stops and prints an error.
 void error_loop(){
   M5.lcd.println("\nFatal error.");
   while(1){
@@ -54,6 +54,7 @@ void error_loop(){
   }
 }
 
+//  Draws a thingy marker to the screen
 void drawMarker(u_int64_t data) {
   int size = HEIGHT / 6;
   int inset = (WIDTH - HEIGHT) / 2;
@@ -68,132 +69,82 @@ void drawMarker(u_int64_t data) {
   }
 }
 
-// void (* rclc_subscription_callback_t)(const void *);
-
-// Implementation example:
-void subscription_callback(const void * msgin)
-{
-  // Cast received message to used type
-  const std_msgs__msg__Float32 * msg = (const std_msgs__msg__Float32 *)msgin;
-
-  char s[32];
-
-  // Process message
-  sprintf(s, "Received: %f\0", msg->data);
-
-  M5.lcd.clear();
-  M5.lcd.drawString(s, 0, 0);
-}
-
-// Implementation example:
+// Handles vector messages recieved from a ROS subscription
 void vector_callback(const void * msgin)
 {
-  // Cast received message to used type
+  //  Cast received message to vector
   const geometry_msgs__msg__Vector3 * msg = (const geometry_msgs__msg__Vector3 *)msgin;
 
+  //  Converts message to a string
   char s[32];
-
-  // Process message
   sprintf(s, "Received: %f\0", msg->x);
 
+  //  Prints message to the screen
   M5.lcd.clear();
   M5.lcd.drawString(s, 0, 0);
 
+  //  Converts message to i2c_status
   i2c_status_tx.x = msg->x;
   i2c_status_tx.y = 0;
   i2c_status_tx.theta = 0;
   i2c_status_tx.status = 0;
 
+  //  Sends i2c_status to the 3Pi
   Wire.beginTransmission(ROBOT_I2C_ADDR);
-  Wire.write( (uint8_t*)&i2c_status_tx, sizeof( i2c_status_tx ));
+  Wire.write((uint8_t*)&i2c_status_tx, sizeof(i2c_status_tx));
   Wire.endTransmission();
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  
-  //M5.begin(true, false, true, true);
+  //  Set up stack
   M5.begin();
+
+  //  Set up serial connection for debugging
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  Wire.begin();  
-  // drawMarker(0x000000056A56A56A);
-  // M5.lcd.println("\nSetting up I2C aaa");
 
-  M5.lcd.drawString("aaaaaaa", 0, 0);
+  //  Set up wire to communicate with 3Pi
+  Wire.begin();
 
-  // M5.lcd.println("\na");
+  //  Draw checkerboard marker to the screen
+  drawMarker(0x000000056A56A56A);
 
+  //  Connect to micro ROS agent
   set_microros_wifi_transports("TP-Link_102C", "35811152", "192.168.0.101", 8888);
 
-  //  M5.lcd.println("b");
-
+  //  Wait a bit, not sure why this is needed but its in the ROS tutorial
   delay(2000);
 
+  //  Set up ROS setup stuff
   allocator = rcl_get_default_allocator();
-
-  //  M5.lcd.println("c");
-
-  //create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  //  M5.lcd.println("d");
-
-  // create node
+  //  Create a ROS node
   RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_wifi_node_2", "", &support));
 
-  //  M5.lcd.println("e");
+  //  Counts the number of handles (subscriptions, timers, etc) being used
+  size_t handle_count = 0;
 
-  // create publisher
-  // RCCHECK(rclc_subscription_init_best_effort(
-  //   &subscriber,
-  //   &node,
-  //   ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-  //   "topic_name"));
-
+  //  Subscribe to the vector ROS topic, using Vector3 messages
   RCCHECK(rclc_subscription_init_best_effort(
     &vector_subscriber,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3),
     "vectors"));
+  handle_count++;
 
-  // msg.data = 0;
+  //  Creates an executor to handle the subscriptions
+  RCCHECK(rclc_executor_init(&executor, &support.context, handle_count, &allocator));
 
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-
-  // RCCHECK(rclc_executor_add_subscription(
-  //   &executor, &subscriber, &msg,
-  //   &subscription_callback, ON_NEW_DATA));
-
+  //  Adds the vector subscription to the executor
   RCCHECK(rclc_executor_add_subscription(
     &executor, &vector_subscriber, &vector_msg,
     &vector_callback, ON_NEW_DATA));
-
-  // M5.lcd.println("\ndone");
 }
 
 void loop() {
-
-  // Setup data to send to robot
-
-
-  // Send an update down to the robot
-
-  // Read values back from the robot.
-  // Serial.println("Read: ");
-  // Wire.requestFrom( ROBOT_I2C_ADDR, sizeof( i2c_status_rx ));
-  // Wire.readBytes( (uint8_t*)&i2c_status_rx, sizeof( i2c_status_rx ));
-  // printRXStatus();
-
-  // RCSOFTCHECK(rcl_publish(&subscriber, &msg, NULL));
-  // msg.data++;
-
-  // M5.lcd.println("\ndid");
-
+  //  Checks for messages from the subscriptions
   RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
-  
-
-  // delay(100);
 }
 
 void printRXStatus() {
